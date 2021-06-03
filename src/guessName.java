fabcf42712275f585c4c46e75185bf245cf1231f
John Young <ynyjcxyz@gmail.com>
2021-06-02 20:24:00 -0700

    On branch main,the win condition is not working right now, need change  the way of the break.

--- src/guessName.java
+++ src/guessName.java
@@ -1,20 +1,58 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class guessName {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("D:/JAVA_file/GuessTheMovie/movies.txt");
+        File file = new File("movies.txt");
         Scanner scanner = new Scanner(file);
-        int line = 0;
-        String[] title = null;
+        int numberOfMovie = 0;
+        List<String> lists = new ArrayList<>();
 
         while(scanner.hasNextLine()){
-            title[line] = scanner.nextLine();
-            line++;
+            String line = scanner.nextLine();
+            //System.out.println(line);
+            lists.add(line);
         }
-        int randomTitleNumber = (int)((line+1)*Math.random());
-        String value = title[randomTitleNumber];
-        System.out.println(value);
+        Object[] array = lists.toArray();
+        //get how many movies this array has
+        numberOfMovie = array.length;
+        //System.out.println(numberOfMovie);
+        //generate a random number from 0 to the length of the array, could be 0 since it's an array unit
+        int randomTitleNumber = (int)( numberOfMovie * Math.random() );
+        //convert Object array[randomTitleNumber] to string variable
+        //String guessingValue = array[randomTitleNumber].toString();
+        String guessingValue = "room";
+        //System.out.println(guessingValue);
+        int length = guessingValue.length();
+        char[]zero = new char[length];
+        for(int n = 0; n < length ; n++){
+            zero[n]=' ';
+        }
+
+        for(int i= 0 ; i<10 ; i++) {
+            System.out.print("Please type a letter to guess:");
+            Scanner myInput = new Scanner(System.in);
+            char letterInput = myInput.next().charAt(0);
+            char[]displayArray = guessingValue.toCharArray();
+            char[]judgeArray = guessingValue.toCharArray();
+            for(int j = 0 ; j<length ; j++){
+                if(letterInput != guessingValue.charAt(j)){
+                    displayArray[j] = '_';
+                }
+                else if(letterInput == guessingValue.charAt(j)){
+                    judgeArray[j] = ' ';
+                }
+            }
+            System.out.println(displayArray);
+            if(Arrays.equals(judgeArray,zero)){
+                System.out.println(Arrays.equals(judgeArray,zero));
+                break;
+            }
+        }
+        System.out.println("Sorry,only 10 times to try");
     }
 }
